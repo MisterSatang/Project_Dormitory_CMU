7fdb69bcdb0203ed68eeff518212c234e04a18b9
Stilllbeaum <narumol.jeenjuhepan@gmail.com>
2022-09-22 01:01:32 +0700

    footer

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -3,40 +3,56 @@ import React from 'react';
 export default function Footer() {
     return (
         <>
-            <footer class="w-100 py-4 flex-shrink-0">
-                <div class="container py-4">
-                    <div class="row gy-4 gx-5">
-                        <div class="col-lg-5 col-md-6">
-                            <img src='https://library.cmu.ac.th/layout/default/images/CMU_WEB/Chiang_Mai_University_Logo.png' height="100" alt="" ></img>
-                            <h6 class="h6 text-white my-2">ที่อยู่</h6>
-                            <p class="small text-muted">สำนักงานหอพักนักศึกษา หอพักนักศึกษาหญิง อาคาร 7239 ถนนห้วยแก้ว ต.สุเทพ อ.เมือง จ.เชียงใหม่ 50200</p>
-                            <p class="small text-muted mb-0">&copy;2019 สำนักงานหอพักนักศึกษา <a class="text-primary" href="#">http://udo.oop.cmu.ac.th</a></p>
-                        </div>
-                        <div class="col-lg-4 col-md-6">
-                            <h5 class="text-white mb-3">เบอร์โทรศัพท์</h5>
-                            <ul class="list-unstyled text-muted">
-                                <li><a class="footer-a">ที่พัก/ทะเบียนราษฎร์ :	053-944749</a></li>
-                                <li><a class="footer-a">กิจกรรม :	053-944750</a></li>
-                                <li><a class="footer-a">การเงิน :	053-944745</a></li>
-                                <li><a class="footer-a">ธุรการ :	053-944747</a></li>
-                                <li><a class="footer-a">บุคคล :	053-944748</a></li>
-                                <li><a class="footer-a">พัสดุ :	053-944751</a></li>
-                                <li><a class="footer-a">Fax :	053-944700</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-3 col-md-6">
-                            <h5 class="text-white mb-3">ช่องทางการติดต่อ</h5>
-                            <ul class="list-unstyled text-muted">
-                                <li><a class="footer-a" href="#">Home</a></li>
-                                <li><a class="footer-a" href="#">About</a></li>
-                                <li><a class="footer-a" href="#">Get started</a></li>
-                                <li><a class="footer-a" href="#">FAQ</a></li>
-                            </ul>
-                        </div>
-                        
+        <footer>
+            <div class="container py-2">
+                <div class="row py-4">
+                    <div class="col-lg-4 col-md-6 mb-4 mb-lg-0"><img src="https://www.cmu.ac.th/content/images/logo_footer.png" alt width={330} className="mb-3" />
+
+
+                        <p class="font-italic text-white">ที่อยู่ : สำนักงานหอพักนักศึกษา อาคาร 7239 ถนนห้วยแก้ว ต.สุเทพ อ.เมือง จ.เชียงใหม่ 50200</p>
+                        <ul class="list-inline mt-1">
+                            <li class="list-inline-item"><a href="#" target="_blank" title="twitter"><i class="text-white bi bi-twitter"></i></a></li>
+                            <li class="list-inline-item"><a href="#" target="_blank" title="facebook"><i class="text-white bi bi-facebook"></i></a></li>
+                            <li class="list-inline-item"><a href="#" target="_blank" title="instagram"><i class="text-white bi bi-instagram"></i></a></li>
+                            <li class="list-inline-item"><a href="#" target="_blank" title="website"><i class=" text-white bi bi-browser-edge me-1"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
+                        <div class="d-flex mb-4 h-2"><h6 class="text-white text-uppercase font-weight-bold">เบอร์โทรศัพท์</h6></div>
+                        <ul class="list-unstyled mb-0">
+                            <li class="mb-2 hover-link"><a href="#" class="text-white text-decoration-none">ที่พัก: 053-944749</a></li>
+                            <li class="mb-2 hover-link"><a href="#" class="text-white text-decoration-none">กิจกรรม: 053-944750</a></li>
+                            <li class="mb-2 hover-link"><a href="#" class="text-white text-decoration-none">การเงิน: 053-944745</a></li>
+                            
+                        </ul>
+                    </div>
+                    <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
+                        <div class="d-flex mb-4 h-2"><h6 class="text-white text-uppercase font-weight-bold"></h6></div>
+                        <ul class="list-unstyled mb-0">
+                            <li class="mb-2 hover-link"><a href="#" class="text-white text-decoration-none">ธุรการ: 053-944747</a></li>
+                            <li class="mb-2 hover-link"><a href="#" class="text-white text-decoration-none">บุคคล: 053-944748</a></li>
+                            <li class="mb-2 hover-link"><a href="#" class="text-white text-decoration-none">พัสดุ: 053-944751</a></li>
+                            <li class="mb-2 hover-link"><a href="#" class="text-white text-decoration-none">Fax: 053-944700</a></li>
+
+                        </ul>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-lg-0">
+                        <h6 class="text-white text-uppercase font-weight-bold mb-4">ช่องทางการติดต่อ</h6>
+                        <ul class="list-unstyled text-muted">
+                                <li class="hover-link"><a class="text-decoration-none text-white footer-a" href="https://www.facebook.com/dormCMU/"><i class="bi bi-facebook me-2 "></i>https://www.facebook.com/dormCMU/</a></li>
+                                <li class="hover-link"><a class="text-decoration-none text-white footer-a" href="https://twitter.com/dormincmu"><i class="bi bi-twitter me-2"></i>https://twitter.com/dormincmu</a></li>
+                                <li class="hover-link"><a class="text-decoration-none text-white footer-a" href="http://udo.oop.cmu.ac.th"><i class="bi bi-browser-edge me-2"></i>http://udo.oop.cmu.ac.th</a></li>
+                        </ul>
                     </div>
                 </div>
-            </footer>
+            </div>
+
+            <div class="bg-light py-2">
+                <div class="container text-center">
+                    <p class="text-muted mb-0 py-2">สำนักงานหอพักนักศึกษา มหาวิทยาลัยเชียงใหม่</p>
+                </div>
+            </div>
+        </footer>
         </>
     );
 }
