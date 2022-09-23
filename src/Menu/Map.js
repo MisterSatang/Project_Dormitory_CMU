79f2db4e6c479cfbe582da43d48b9782ee0f5534
ammaret909 <ammaret_aituphon@cmu.ac.th>
2022-09-23 19:45:46 +0700

    SS-News-and-Edit-Map

--- src/Menu/Map.js
+++ src/Menu/Map.js
@@ -6,20 +6,23 @@ export default function App() {
         <>
             < Navbar />
             <div className="container-fluid p-0 m-0 bg-map">
-                <div className="row">
-                    <div className="col-1">
-                    </div>
+                <div className="row justify-content-center">
+                    <div className="col-1"></div>
+
                     <div className="col-10">
                         <div className="d-flex h-contain-map justify-content-center p-4 marjin15 ">
-                            <div className="d-flex bg-white shadow rounded-4 p-4 ">
+                            <div className="d-flex bg-white shadow rounded-4 ">
                                 <img src="img_dormitory/Map/map_dorm.jpg" className="contain-map photo-map col-12 d-flex " />
-                        <div className="d-flex  justify-content-center p-4 marjin15">
-                            <div className="d-flex bg-white shadow rounded-4 p-4 ">
-                                <img src="img_dormitory/Map/map_dorm.jpg" className="contain-map h-contain-map" />
+                                <div className="d-flex  justify-content-center p-4 marjin15">
+                                    <div className="d-flex shadow rounded-4 ">
+                                        <img src="img_dormitory/Map/map_dorm.jpg" className="contain-map h-contain-map" />
+                                    </div>
+                                </div>
                             </div>
+
+                            <div className="col-1"></div>
                         </div>
                     </div>
-                    <div className="col-1"></div>
                 </div>
             </div>
             < Footer />
