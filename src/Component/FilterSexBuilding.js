e20fce8e170dce2fb1c1b0f96f8b28c731cd48eb
Satang Budsai <Satang_budsai@cmu.ac.th>
2022-09-30 01:02:06 +0700

    Booking cardRoom

--- src/Component/FilterSexBuilding.js
+++ src/Component/FilterSexBuilding.js
@@ -1,23 +1,40 @@
-export default function Item_dropdown_filter({setSexBuilding,dormitories}) {
-  
-  const onChange = (e) => {
-    setSexBuilding(e.target.value)
-  }
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-  return(
-    <>
-      <select onChange={onChange} class="form-select mb-3" aria-label="Default select example">
-      <option>
-          - 
-      </option>
-      {
-        dormitories.filter(dormitory => dormitory.sex.includes('builderMale')).map(filteredDormitory => (
-          <option key={filteredDormitory.id} value={filteredDormitory.name} item={filteredDormitory}>
-            {filteredDormitory.name}
-          </option>
-        ))
+export default function Item_dropdown_filter({ setSexBuilding }) {
+  const [dormitories, setDormitories] = useState([]);
+
+  useEffect(() => {
+    async function getDormitories() {
+      try {
+        const dormitory = await axios.get("http://localhost:8000/dormitories");
+        // console.log(dormitory.data);
+        setDormitories(dormitory.data);
+      } catch (error) {
+        console.error(error);
       }
+    }
+    getDormitories();
+  }, []);
 
+  const onChange = (e) => {
+    setSexBuilding(e.target.value);
+  };
+
+  return (
+    <>
+      <select
+        onChange={onChange}
+        class="form-select mb-3"
+        aria-label="Default select example"
+      >
+        {dormitories
+          .filter((dormitory) => dormitory.sex.includes("builderMale"))
+          .map((filteredDormitory) => (
+            <option key={filteredDormitory.id} value={filteredDormitory.id}>
+              {filteredDormitory.name}
+            </option>
+          ))}
       </select>
     </>
   );
--- src/Menu/Booking.js
+++ src/Menu/Booking.js
@@ -1,106 +1,138 @@
-import React, { useState, useEffect} from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import Navbar from '../Component/Nav';
-import Footer from '../Component/Footer';
-import FilterSexBuilding from '../Component/FilterSexBuilding';
-import FilterFloorBuilding from '../Component/FilterFloorBuilding';
-import FilterEmptyPeople from '../Component/FilterEmptyPeople';
-import Select_room from '../Component/Select_room';
-import Select_room_full from '../Component/Select_room_full';
+import Navbar from "../Component/Nav";
+import Footer from "../Component/Footer";
+import FilterSexBuilding from "../Component/FilterSexBuilding";
+import FilterFloorBuilding from "../Component/FilterFloorBuilding";
+import Select_room from "../Component/Select_room";
 
 export default function App() {
-
   const [dormitories, setDormitories] = useState([]);
 
-  const [sexBuilding, setSexBuilding] = useState([]);
+  const [sexBuilding, setSexBuilding] = useState(3);
   const [floorBuilding, setFloorBuilding] = useState([]);
-  const [EmptyPeople, setEmptyPeople] = useState([]);
+  const [cardRoom, setCardRoom] = useState([]);
 
-  // console.log(sexBuilding)
-  // console.log(floorBuilding)
-  // console.log(EmptyPeople)
+  useEffect(() => {
+    async function getDormitories() {
+      try {
+        let setRoom = [];
+        // console.log({ sexBuilding });
 
-  useEffect(() =>{
-      async function getDormitories() {
-        try {
-          const dormitory = await axios.get('http://localhost:8000/dormitories');
-          console.log(dormitory.data);
-          setDormitories(dormitory.data);
-        } catch (error) {
-          console.error(error);
-        }
+        sexBuilding > 0
+          ? setRoom.push(`sex=${sexBuilding}`)
+          : setRoom.push(null);
+        floorBuilding > 0
+          ? setRoom.push(`floor=${floorBuilding}`)
+          : setRoom.push(null);
+
+        // console.log({ setRoom });
+
+        const room =
+          setRoom.filter((v) => v != null).length === 0
+            ? ""
+            : `?${setRoom.filter((v) => v != null).join("&")}`;
+
+        // console.log(room);
+
+        const dormitory = await axios.get(
+          `http://localhost:8000/dormitories${room}`
+        );
+        // console.log(dormitory.data);
+        setDormitories(dormitory.data);
+      } catch (error) {
+        console.error(error);
       }
-      getDormitories();
-  },[]);
+    }
+    getDormitories();
+  }, [sexBuilding, floorBuilding, cardRoom]);
 
   return (
     <>
-    < Navbar />
+      <Navbar />
 
-    <div class="d-flex">
-        <img src="img_dormitory/Select_room/11.jpg" alt="Bg-cmu" className="w-100 image-bg-select_room" />
-    </div>
-    <div class="container-fluid p-0">
-        <div class="row mt-4 mx-4">
-            <div class="col-xl-3 col-lg-4 col-md-5 col-12 col-sm-6 pe-4 ps-0">
-                <div class="bg-white shadow rounded-top-4">
-                    <div class="pt-4 px-3  bg-light rounded-top-4 border-bottom border-1 border-3">
-                        <div class="d-flex align-items-center"><img src="img_dormitory/imageStudent/01.jpg" alt="image" className="mr-3 profile-student shadow-sm rounded-3" />
-                        <div class="ms-3">
-                            <div class="d-inline-flex fs-5 m-0 me-2">สตางค์</div>
-                            <div class="d-inline-flex fs-5 m-0">บุตรสัย</div>
-                            <div class="font-weight-light text-muted mb-0">คณะ ศิลปะ สื่อ เทคโนโลยี</div>
-                            <div class="font-weight-light text-muted mb-0">สาขา DII</div>
-                        </div>
-                        </div>
-                        <div class="py-3 text-center text-gray">
-                          ไม่มีการจองห้องพัก
-                        </div>
+      <div className="d-flex">
+        <img
+          src="img_dormitory/Select_room/11.jpg"
+          alt="Bg-cmu"
+          className="w-100 image-bg-select_room"
+        />
+      </div>
+      <div className="container-fluid p-0">
+        <div className="row mt-4 mx-4">
+          <div className="col-xl-3 col-lg-4 col-md-5 col-12 col-sm-6 pe-4 ps-0">
+            <div className="bg-white shadow rounded-top-4">
+              <div className="pt-4 px-3  bg-light rounded-top-4 border-bottom border-1 border-3">
+                <div className="d-flex align-items-center">
+                  <img
+                    src="img_dormitory/imageStudent/01.jpg"
+                    alt="imageStudent"
+                    className="mr-3 profile-student shadow-sm rounded-3"
+                  />
+                  <div className="ms-3">
+                    <div className="d-inline-flex fs-5 m-0 me-2">สตางค์</div>
+                    <div className="d-inline-flex fs-5 m-0">บุตรสัย</div>
+                    <div className="font-weight-light text-muted mb-0">
+                      คณะ ศิลปะ สื่อ เทคโนโลยี
                     </div>
-                  
-
-                <div class="p-1 bg-light rounded-r-4 shadow-sm mb-4">
-                  <div class="input-group">
-                  <input type="search" placeholder="ค้นหา : หมายเลขห้อง ?" aria-describedby="button-addon1" class="form-control border-0 bg-light" />
-                    <div class="input-group-append">
-                      <button id="button-addon1" type="submit" class="btn btn-link text-dark"><i class="bi bi-search"></i></button>
+                    <div className="font-weight-light text-muted mb-0">
+                      สาขา DII
                     </div>
                   </div>
                 </div>
-                <p class="text-gray font-weight-bold text-uppercase px-3 small pb-3 mb-0"><i class="bi bi-funnel me-1"></i>Filter</p>
+                <div className="py-3 text-center text-gray">
+                  ไม่มีการจองห้องพัก
+                </div>
+              </div>
 
-                <ul class="nav flex-column bg-light mb-0 px-4 py-3 rounded-ts-4">
-                    <li class="nav-item">
-                      <div class="mb-1">หอพัก</div>
-                  <FilterSexBuilding setSexBuilding={setSexBuilding} dormitories={dormitories} />
-                    </li>
-                    <li class="nav-item">
-                      <div class="mb-1">ชั้น</div>
-                      <FilterFloorBuilding setFloorBuilding={setFloorBuilding} />
-                    </li>
-                    <li class="nav-item">
-                      <div class="mb-1">จำนวนคนว่าง</div>
-                      <FilterEmptyPeople setEmptyPeople={setEmptyPeople}/>
-                    </li>
-                </ul>
+              <div className="p-1 bg-light rounded-r-4 shadow-sm mb-4">
+                <div className="input-group">
+                  <input
+                    type="search"
+                    placeholder="ค้นหา : หมายเลขห้อง ?"
+                    aria-describedby="button-addon1"
+                    className="form-control border-0 bg-light"
+                  />
+                  <div className="input-group-append">
+                    <button
+                      id="button-addon1"
+                      type="submit"
+                      className="btn btn-link text-dark"
+                    >
+                      <i className="bi bi-search"></i>
+                    </button>
+                  </div>
                 </div>
+              </div>
+              <p className="text-gray font-weight-bold text-uppercase px-3 small pb-3 mb-0">
+                <i className="bi bi-funnel me-1"></i>Filter
+              </p>
+
+              <ul className="nav flex-column bg-light mb-0 px-4 py-3 rounded-ts-4">
+                <li className="nav-item">
+                  <div className="mb-1">หอพัก</div>
+                  <FilterSexBuilding setSexBuilding={setSexBuilding} />
+                </li>
+                <li className="nav-item">
+                  <div className="mb-1">ชั้น</div>
+                  <FilterFloorBuilding setFloorBuilding={setFloorBuilding} />
+                </li>
+              </ul>
             </div>
-            <div class="col p-0">
-                <div class="row m-0">
-                  <Select_room />
-                  <Select_room_full />
-                  <Select_room />
-                  <Select_room />
-                  <Select_room_full />
-                  <Select_room />
-                  <Select_room />
-                  <Select_room />
-                </div>
+          </div>
+          <div className="col p-0">
+            <div className="row m-0">
+              <Select_room
+                dormitories={dormitories}
+                setCardRoom={setCardRoom}
+                sexBuilding={sexBuilding}
+              />
             </div>
+          </div>
         </div>
-    </div>
-      < Footer />
+      </div>
+      <Footer />
     </>
   );
 }
